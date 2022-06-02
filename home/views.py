f40aa76527254286290f5ea4bac2ddbe6082e37e
Soyad Mohiful Islam <mohiful.islam.ofc@gmail.com>
2022-06-02 12:29:17 +0600

    fix blog url issue

--- home/views.py
+++ home/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import get_object_or_404, redirect, render
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout
 from django.contrib import messages
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render
+
 from .form import *
 
 
@@ -36,9 +37,10 @@ def home(request):
 def category(request, slug):
     allblogs = BlogModel.objects.filter(category__slug=slug)
     categories = Category.objects.all()
-    context =  { 'allblogs': allblogs,
-                 'categories': categories,
-                }
+    context =  { 
+                'allblogs': allblogs,
+                'categories': categories,
+        }
     return render (request, 'blog/category.html', context)
 
 
